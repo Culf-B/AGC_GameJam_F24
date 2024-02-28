5dc99f408d61e8be7ea34478e739089160bcc0a7
Culf-B <49363745+Culf-B@users.noreply.github.com>
2024-02-28 14:16:40 +0100

    Started working on the game

--- sketch.js
+++ sketch.js
@@ -1,5 +1,37 @@
+class Level {
+  constructor(path) {
+    this.loadLevel(path);
+    this.file = undefined;
+    this.size = undefined;
+    this.tileSet = undefined;
+    this.tiles = undefined;
+
+    this.loadingStatus = false; // True when done loading
+  }
+  loadLevel(path) {
+    loadJSON(path, (file) => {
+      this.file = file;
+      this.loadingStatus = true;
+    })
+  }
+
+  isDoneLoading() {
+    return this.loadingStatus;
+  }
+}
+
+class TileSet {
+  constructor(rawTileSet) {
+    this.tileSet = rawTileSet
+    this.tileSet.forEach(tile => {
+      tile.image = loadImage(tile.image);
+    });
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  new Level("test.json");
 }
 
 function draw() {
